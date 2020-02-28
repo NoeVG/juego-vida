bccda723b61c8a443a09d2e7ba6fbea4d28c618d
Noe <noe@localhost.localdomain>
2020-02-28 11:04:41 -0600

    remove try exception

--- juego.py
+++ juego.py
@@ -10,98 +10,65 @@ from random import seed
 from random import randint
 
 limiteMax = 100
-n = 0
-b = 255
 
+n = 1
+b = 255
 
 def checkCellLives(img,x,y):
     cellLives = 0
     'check x+1 cell'
-    try:
-        if img[x+1][y] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
+    if img[x+1][y] == n:
+        cellLives +=1
     'check x-1 cell'
-    try:
-        if img[x-1][y] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
+    if img[x-1][y] == n:
+        cellLives +=1
     'check y+1 cell'
-    try:
-        if img[x][y+1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
+    if img[x][y+1] == n:
+        cellLives +=1
     'check y-1 cell'
-    try:
-        if img[x][y-1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
-    pass
+    if img[x][y-1] == n:
+        cellLives +=1
     'check x+1/y-1 cell'
-    try:
-        if img[x+1][y-1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
-    pass
+    if img[x+1][y-1] == n:
+        cellLives +=1
     'check x+1/y+1 cell'
-    try:
-        if img[x+1][y+1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
-    pass
+    if img[x+1][y+1] == n:
+        cellLives +=1
     'check x-1/y-1 cell'
-    try:
-        if img[x-1][y-1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
-    pass
+    if img[x-1][y-1] == n:
+        cellLives +=1
     'check x-1/y+1 cell'
-    try:
-        if img[x-1][y+1] == n:
-            cellLives +=1
-        pass
-    except IndexError:
-        pass
+    if img[x-1][y+1] == n:
+        cellLives +=1
     return cellLives
 
 img = np.zeros((limiteMax, limiteMax, 1), dtype = "uint8")
 
 print('Press Ctrl-C to quit.')
 try:
-    for x in range(len(img)):
-        for y in range(len(img)):
+    for x in range(1,len(img)-1):
+        for y in range(1,len(img)-1):
             img[x][y] = b
             pass
         pass
     'Initialize enviroment'
+
     start = 0
     while start < 400:
-        x = randint(0,limiteMax-1)
-        y = randint(0,limiteMax-1)
+        x = randint(1,limiteMax-2)
+        y = randint(0,limiteMax-2)
         img[x,y] = n
         start+=1
+
+
     while True:
         #x = randint(0,limiteMax-1)
         #y = randint(0,limiteMax-1)
-        for x in range(len(img)):
-            for y in range(len(img)):
+        for x in range(1,len(img)-1):
+            for y in range(1,len(img)-1):
                 if img[x][y] == n:
                     cellLives = checkCellLives(img,x,y)
-                    print(cellLives)
+                    #print(cellLives)
 
                     if cellLives == 2:
                         img[x][y] = n
@@ -111,7 +78,7 @@ try:
                         img[x][y] = b
                 elif img[x][y] == b:
                     cellLives = checkCellLives(img,x,y)
-                    print(cellLives)
+                    #print(cellLives)
 
                     if cellLives == 3:
                         img[x][y] = n
@@ -131,16 +98,16 @@ try:
         if totalCellLives == 0:
             print("Enviroment Fail!")
             cv2.namedWindow('image',10)
-            cv2.resizeWindow('image', 600,600)
+            cv2.resizeWindow('image', 1200,1200)
             cv2.imshow('image',img)
             cv2.waitKey(0)
             input()
             break
         else:
             cv2.namedWindow('image',10)
-            cv2.resizeWindow('image', 600,600)
+            cv2.resizeWindow('image', 800,800)
             cv2.imshow('image',img)
-            cv2.waitKey(1)
+            cv2.waitKey(4)
 except KeyboardInterrupt:
     print('\n')
 
